71e7b2efa04cde35f90c51d5c32fee8d23fcc742
boBObiza1!! <a00232432@tec.mx>
2025-03-18 21:39:07 -0600

    VERSION YA NO SE QUE

---
+++ Programa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <math.h>
+
+int main(){
+	int length
+	scanf("%i, &length);
+	
+	int office = pow(2, length);
+	printf("%i", office);
+	return 0;
+}
